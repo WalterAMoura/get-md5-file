8af936ce5fc6a47c4fd21c29b236dc00d087a2d5
WalterAMoura <96154136+WalterAMoura@users.noreply.github.com>
2022-02-23 16:12:36 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,11 @@
 <?php
 
-    
-
-    //use Source\Models\UUID as uuid;
-    require_once(__DIR__ . '/source/Models/UUID.php');
-
     $path = $_GET['path'] ?? -1;
     $file = $_FILES['file'] ?? -1;
     $dirUpload = "./upload";
+
     if($path == 'upload'){
-        //$fileName=$uuid->getGUID();
-        $uuid = new \Source\Models\UUID();
-        $fileName=$uuid->getGUID();
+        $fileName= getUUID();
         if(move_uploaded_file($file["tmp_name"], "$dirUpload/".$fileName)){
             $filemd5=md5_file("$dirUpload/".$fileName);
             $response=array(
@@ -27,4 +21,21 @@
         $errors=array("error"=>"No Route matched with those values.");
         header('Content-Type: application/json; charset=utf-8');
         echo json_encode($errors);
+    }
+
+    function getUUID(){
+        if (function_exists('com_create_guid')){
+            return com_create_guid();
+        }else{
+            mt_srand((double)microtime()*10000);
+            $charid = strtoupper(md5(uniqid(rand(), true)));
+            $hyphen = chr(45);// "-"
+            $uuid = ""
+                .substr($charid, 0, 8).$hyphen
+                .substr($charid, 8, 4).$hyphen
+                .substr($charid,12, 4).$hyphen
+                .substr($charid,16, 4).$hyphen
+                .substr($charid,20,12);
+            return $uuid;
+        }
     }
