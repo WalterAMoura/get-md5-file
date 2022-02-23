2db5dc111008197ea599fa9d8fc5baeb93d62553
WalterAMoura <96154136+WalterAMoura@users.noreply.github.com>
2022-02-23 16:07:02 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,22 +1,23 @@
 <?php
 
     
+
     //use Source\Models\UUID as uuid;
-    //require_once(__DIR__ . '/source/Models/UUID.php');
+    require_once(__DIR__ . '/source/Models/UUID.php');
 
     $path = $_GET['path'] ?? -1;
     $file = $_FILES['file'] ?? -1;
-    $dirUpload = __DIR__ . "/upload";
+    $dirUpload = "./upload";
     if($path == 'upload'){
-        //$uuid = new uuid();
         //$fileName=$uuid->getGUID();
-
-        if(move_uploaded_file($file["tmp_name"], "$dirUpload/".$file["name"])){
-            $filemd5=md5_file("$dirUpload/".$file["name"]);
+        $uuid = new \Source\Models\UUID();
+        $fileName=$uuid->getGUID();
+        if(move_uploaded_file($file["tmp_name"], "$dirUpload/".$fileName)){
+            $filemd5=md5_file("$dirUpload/".$fileName);
             $response=array(
                 "file_md5"=> $filemd5
             );
-            unlink("$dirUpload/".$file["name"]);
+            unlink("$dirUpload/".$fileName);
             http_response_code(200);
             header('Content-Type: application/json; charset=utf-8');
             echo json_encode($response);
